ce48fa31118ffd84c4d3b24fe3db28e818cc3840
Krystian Wierciak <krystianwierciak@MacBook-Pro-von-Krystian.local>
2025-02-02 23:09:40 +0100

    Allowed host to live version.

--- videos/settings.py
+++ videos/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = config('DEBUG', default=False, cast=bool)
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://videos-ym87.onrender.com/", "127.0.0.1"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'videos-ym87.onrender.com']
+
 
 
 # Application definition
